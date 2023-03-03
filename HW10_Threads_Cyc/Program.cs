47e96cac9a5ed9b2a9e1e4cd6bb8609e05df5f29
urt <urt5002urt@gmail.com>
2023-03-03 23:05:04 +0200

    add readme file

---
+++ HW10_Threads_Cyc/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HW10_Threads_Cyc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
